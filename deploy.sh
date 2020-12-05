ab4ef2bc614225f9f783e286069bf7672bab4a17
DESKTOP-CNU6H97\Raeme <cieonsers@gmail.com>
2020-12-06 02:58:50 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -204,7 +204,7 @@ api_old(){
 	#replay dir
 	WEBAPI_URL=${WEBAPI_URL:-"http://${ip}"}
 	sed -i '/WEBAPI_URL/c \WEBAPI_URL = '\'${WEBAPI_URL}\''' "/root/${NODE_LIST}/userapiconfig.py"
-	WEBAPI_TOKEN=${WEBAPI_TOKEN:-"marisn"}
+	WEBAPI_TOKEN=${WEBAPI_TOKEN:-"mupass"}
 	sed -i '/WEBAPI_TOKEN/c \WEBAPI_TOKEN = '\'${WEBAPI_TOKEN}\''' "/root/${NODE_LIST}/userapiconfig.py"
 	NODE_ID=${NODE_ID:-"3"}
 	sed -i '/NODE_ID/c \NODE_ID = '${NODE_ID}'' "/root/${NODE_LIST}/userapiconfig.py"
